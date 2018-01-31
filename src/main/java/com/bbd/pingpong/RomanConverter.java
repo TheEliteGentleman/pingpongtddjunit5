69655958047c931991e3685d0b8d0a68299152ed
Buhake Sindi <buhake.sindi@sindi.co.za>
2018-01-31 20:19:23 +0200

    formatting....

--- src/main/java/com/bbd/pingpong/RomanConverter.java
+++ src/main/java/com/bbd/pingpong/RomanConverter.java
@@ -7,10 +7,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class RomanConverter {
-	
+
 	private static final Map<Character, Integer> ROMAN_NUMERALS;
 	private static Set<String> SUBTRACTIVE_NUMERALS;
-	
+
 	static {
 		Map<Character, Integer> romanNumerals = new HashMap<>();
 		romanNumerals.put('I', 1);
@@ -20,9 +20,9 @@ public class RomanConverter {
 		romanNumerals.put('C', 100);
 		romanNumerals.put('D', 500);
 		romanNumerals.put('M', 1000);
-		
+
 		ROMAN_NUMERALS = Collections.unmodifiableMap(romanNumerals);
-		
+
 		Set<String> subtractiveNumerals = new HashSet<>();
 		subtractiveNumerals.add("IV");
 		subtractiveNumerals.add("IX");
@@ -30,42 +30,41 @@ public class RomanConverter {
 		subtractiveNumerals.add("XC");
 		subtractiveNumerals.add("CD");
 		subtractiveNumerals.add("CM");
-		
+
 		SUBTRACTIVE_NUMERALS = Collections.unmodifiableSet(subtractiveNumerals);
 	}
-	
-    public static int Convert(String s)
-    {
-    	if (s == null || s.isEmpty())  {
-    		throw (new IllegalArgumentException("Not a valid roman"));
-		}		     
-    		 		     
+
+	public static int Convert(String s) {
+		if (s == null || s.isEmpty()) {
+			throw (new IllegalArgumentException("Not a valid roman"));
+		}
+
 		// Handle upper case and lower case
 		s = s.toUpperCase();
-    	
+
 		int total = 0;
-    	int length = s.length();
-    	
-    	for (int i = 0; i < length; i++) {
-    		char c = s.charAt(i);
-    		int value = toNumeric(c);
-    		boolean subtract = ((i < length - 1) && isSubtractiveNumeral(String.valueOf(new char[] {c, s.charAt(i + 1)})));
-    		
-    		total += subtract ? -value : value;
-    	}
-    	
-        return total;
-    }
-    
-    private static boolean isSubtractiveNumeral(final String s) {
-    	return SUBTRACTIVE_NUMERALS.contains(s);
-    }
-    
-    private static int toNumeric(final char c) {
-    	if (!ROMAN_NUMERALS.containsKey(c)) {
-    		throw new ArithmeticException("Invalid roman character '" + c + "'.");
-    	}
-    	
-    	return ROMAN_NUMERALS.get(c);
-    }
+		int length = s.length();
+
+		for (int i = 0; i < length; i++) {
+			char c = s.charAt(i);
+			int value = toNumeric(c);
+			boolean subtract = ((i < length - 1) && isSubtractiveNumeral(String.valueOf(new char[] { c, s.charAt(i + 1) })));
+
+			total += subtract ? -value : value;
+		}
+
+		return total;
+	}
+
+	private static boolean isSubtractiveNumeral(final String s) {
+		return SUBTRACTIVE_NUMERALS.contains(s);
+	}
+
+	private static int toNumeric(final char c) {
+		if (!ROMAN_NUMERALS.containsKey(c)) {
+			throw new ArithmeticException("Invalid roman character '" + c + "'.");
+		}
+
+		return ROMAN_NUMERALS.get(c);
+	}
 }
